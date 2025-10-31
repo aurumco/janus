25190a2c977f17afc105761b1bf386ce99dedee0
Aurum <aurumos@outlook.com>
2025-10-31 14:28:00 +0330

    Delete dataset directory

--- dataset/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Entry point for dataset creation module."""
-
-try:
-    from .create_multi_asset_dataset import main
-except ImportError:
-    from create_multi_asset_dataset import main
-
-if __name__ == '__main__':
-    main()
